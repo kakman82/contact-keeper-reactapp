d2cc3e960c01625e1c3a2a58de9b8c9d6117d5f5
Kerem Akman <41956184+kakman82@users.noreply.github.com>
2021-11-05 00:02:15 +0300

    Prepare for deploy

--- server.js
+++ server.js
@@ -3,22 +3,33 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-// Connect to DB;
+//* Connect to DB;
 connectDB();
 
-// Init Middleware;
+//* Init Middleware;
 app.use(express.json({ extended: false }));
 
 //! bu kısım ilk server kurulumunda test için yaptık, heroku deploy öncesi commente almak gerek yoksa home page bu görünür :)
 //app.get('/', (req, res) => res.json({ msg: 'Welcome to ContactKeeper API!' }));
 
-// Define Routes;
+//* Define Routes;
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//* Serve static assets - client/build/index.html for production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  //! '*' means all routes and must be after definin routes section above
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
